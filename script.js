0468e653b0ffb5f978dec0254af3941d63ee60f2
HiiIamDev <mribrahim572@gmail.com>
2024-03-04 22:08:52 +0700

    main

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function navBar() {
     var menu = document.querySelector('.nav-res-container')
     menu.classList.toggle('show');
 }
+
+// function hiddenNav() {
+//     var navbar = document.querySelector('.nav-res-container')
+//     if (window.innerWidth >= 780) {
+//         console.log('d1')
+//         if (navbar.style.display === 'block') {
+//             console.log('d2')
+//             navbar.style.display = 'none'
+//         }
+//     }
+// }
+
+// hiddenNav()
